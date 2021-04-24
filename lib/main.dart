c8330e0e462bdfb5f6a3f5b0ec7e371a20c61852
iwashi <iwase.yoshimasa@gmail.com>
2021-04-25 01:11:44 +0900

    image test

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,16 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Center(
-      child: Text('Hello World'),
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('I am Rich'),
+        backgroundColor: Colors.blueGrey[900],
+      ),
+      body: Center(
+        child: Image(
+          image: NetworkImage('https://www.waseda.jp/top/assets/themes/waseda-template-engine-main/img/logo-header-off.png')
+        ),
+      ),
     ),
   ));
 }
